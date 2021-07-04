3dbc7a8455fffefe2980b1e51b4a8389779fbcaf
Roman <romakhram@metaprime.ru>
2021-07-04 13:18:57 +0300

    sirena

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer
+      absolute
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Sirena'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+<v-row>
+  <v-col class="text-center">
+    <v-text-field
+      label="Multitab Text Field"
+      v-model="textField"
+    ></v-text-field>
+  </v-col>
+</v-row>
+</template>
+
+<script>
+export default {
+  mounted() {
+    if (!process.server) {
+      this.activateStorage();
+    };
+  },
+  data: () => ({
+    textField: null
+  }),
+  watch: {
+    textField(val) {
+      localStorage.setItem('forInput', val);
+    }
+  },
+  methods: {
+    activateStorage() {
+      window.addEventListener('storage', (e) => {
+        this.textField = localStorage.getItem('forInput');
+      });
+    }
+  }
+}
+</script>
